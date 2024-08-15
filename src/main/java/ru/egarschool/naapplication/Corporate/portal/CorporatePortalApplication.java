98e768a5d4f0a6e728d276052c09622db2016aef
G4unterKingsman <nagato.200209@gmail.com>
2024-08-15 21:26:52 +0500

    first commit

---
+++ src/main/java/ru/egarschool/naapplication/Corporate/portal/CorporatePortalApplication.java
@@ -0,0 +1,13 @@
+package ru.egarschool.naapplication.Corporate.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporatePortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorporatePortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/egarschool/naapplication/Corporate/portal/CorporatePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.egarschool.naapplication.Corporate.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorporatePortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
