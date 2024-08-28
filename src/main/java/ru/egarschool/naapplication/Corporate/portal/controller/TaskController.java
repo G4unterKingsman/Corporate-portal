bc83ba54c5fcf09afbefaa54df66aaa73a0b80d7
G4unterKingsman <nagato.200209@gmail.com>
2024-08-28 20:26:58 +0500

    added new end-points,DTOs,Mappers added edit Order, and Task

--- src/main/java/ru/egarschool/naapplication/Corporate/portal/controller/TaskController.java
+++ src/main/java/ru/egarschool/naapplication/Corporate/portal/controller/TaskController.java
@@ -5,13 +5,16 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import ru.egarschool.naapplication.Corporate.portal.dto.TaskDto;
 import ru.egarschool.naapplication.Corporate.portal.entity.TaskEntity;
+import ru.egarschool.naapplication.Corporate.portal.mapper.TaskMapper;
 import ru.egarschool.naapplication.Corporate.portal.service.TaskServiceImpl;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("tasks")
+@RequestMapping("all_tasks")
 public class TaskController {
     private final TaskServiceImpl taskService;
 
@@ -33,7 +36,7 @@ public class TaskController {
     @PostMapping("/add_task")
     public String saveTask(@Valid @ModelAttribute TaskEntity taskEntity, Long idWhoGave, Long idWhoGiven){
         taskService.create(taskEntity, idWhoGave, idWhoGiven);
-        return "redirect:/tasks";
+        return "redirect:/all_tasks";
     }
 
     @GetMapping("/{taskId}")
@@ -42,4 +45,26 @@ public class TaskController {
         return "show_task";
     }
 
+
+
+
+    @GetMapping("/{id}/edit_task")
+    public String getEditForm(Model model, @PathVariable Long id){
+        TaskEntity task = taskService.findById(id);
+        model.addAttribute("task", task);
+        TaskDto taskDto = TaskMapper.getTaskDto(task);
+        model.addAttribute("taskDto",taskDto);
+        return "edit_task";
+    }
+
+    @PostMapping("/{id}/edit_task")
+    public String update(@Valid @ModelAttribute TaskDto taskDto, BindingResult bindingResult,
+                         @PathVariable Long id, Model model){
+        TaskEntity task = taskService.findById(id);
+        model.addAttribute("task",task);
+        if(bindingResult.hasErrors())
+            return "edit_task";
+        taskService.update(task, taskDto);
+        return "redirect:/all_tasks/{id}";
+    }
 }
